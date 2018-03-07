7a0fb5bbccc5b6ceaeee6ae49277f9ace6bb2f47
ipopovych <32884361+ipopovych@users.noreply.github.com>
2018-03-07 16:54:23 +0200

    improved field generation

--- funcs.py
+++ funcs.py
@@ -1,48 +1,15 @@
 import random
 from ship import Ship
 
+
 def create_field():
-    f = create_empty_field()
+    f = empty_field()
+    ships = []
+    left_coordinates = []
+
     for i in range(10):
-        a = []
-        for i in range(10):
-            a.append(None)
-        f.append(a)
-
-    coordinates = []
-    for i in range(1, 11):
-        for a in range(1, 11):
-            coordinates.append((i, a))
-
-    present_ships = set()
-
-    def surrounding_cells(cell):
-        x, y = cell[0], cell[1]
-        if x == 1:
-            if y == 1:
-                return [cell, (x, y + 1), (x + 1, y), (x + 1, y + 1)]
-            elif y == 10:
-                return [cell, (x + 1, y), (x, y - 1), (x + 1, y - 1)]
-            else:
-                return [cell, (x + 1, y), (x, y - 1),
-                        (x, y + 1), (x + 1, y + 1), (x + 1, y - 1)]
-
-        elif x == 10:
-            if y == 1:
-                return [cell, (x - 1, y), (x - 1, y + 1), (x, y + 1)]
-            elif y == 10:
-                return [cell, (x, y - 1), (x - 1, y - 1), (x - 1, y)]
-            else:
-                return [cell, (x - 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1)]
-
-        elif y == 1:
-            return [cell, (x - 1, y), (x + 1, y), (x, y + 1), (x - 1, y + 1), (x + 1, y + 1)]
-        elif y == 10:
-            return [cell, (x - 1, y), (x + 1, y), (x, y - 1),
-                    (x - 1, y - 1), (x + 1, y - 1)]
-        else:
-            return [cell, (x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1), (x - 1, y - 1), (x - 1, y + 1),
-                    (x + 1, y + 1), (x + 1, y - 1)]
+        for a in range(10):
+            left_coordinates.append((i, a))
 
     def locate_ship(length):
 
@@ -50,79 +17,52 @@ def create_field():
             for cell in ship_cells:
                 sur = surrounding_cells(cell)
                 for cell in sur:
-                    if cell not in present_ships:
-                        continue
+                    if f[cell[0]][cell[1]] is None:
+                        pass
                     else:
                         return False
             return True
 
-        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         while True:
-            horiz = random.choice((True, False))
-            if horiz:
-                bowraw = random.choice(a)
-                bowcol = random.randint(1, 11 - length)
-
-            else:
-                bowraw = random.randint(1, 11 - length)
-                bowcol = random.choice(a)
-
-            cell = (bowraw, bowcol)
+            found = False
+            while found is False:
+                horiz = random.choice((True, False))
+                cell = random.choice(left_coordinates)
+                if horiz:
+                    bowcol = cell[1]
+                    if bowcol <= 10 - length:
+                        found = True
+                else:
+                    bowraw = cell[0]
+                    if bowraw <= 10 - length:
+                        found = True
 
             s = Ship(length, bow=cell, horizontal=horiz)
-            cells = s.coordinates()
+            shipcells = s.coordinates()
 
-            if check_placing(cells):
-                for cell in cells:
-                    f[cell[0] - 1][cell[1] - 1] = s
-                    for c in surrounding_cells(cell):
-                        present_ships.add(c)
+            if check_placing(shipcells):
+                for cell in shipcells:
+                    f[cell[0]][cell[1]] = s
                 break
             else:
                 continue
-        return cells
+        ships.append(s)
+        return s
 
+    # Locating required ships
     locate_ship(4)
     locate_ship(3)
     locate_ship(3)
     locate_ship(2)
     locate_ship(2)
     locate_ship(2)
-
-    coordinates = []
-    for i in range(1, 11):
-        for a in range(1, 11):
-            coordinates.append((i, a))
-
-    for c in present_ships:
-        if c in coordinates:
-            coordinates.remove(c)
-
-    def check_cell(c):
-        sur = surrounding_cells(c)
-        for cell in sur:
-            if cell not in present_ships:
-                continue
-            else:
-                return False
-
-        return True
-
-    def place_one():
-        for c in coordinates:
-            if check_cell(c):
-                f[c[0] - 1][c[1] - 1] = Ship(1, bow=c, horizontal=True)
-                for cel in surrounding_cells(c):
-                    coordinates.remove(cel)
-                    break
-
     for i in range(4):
-        place_one()
+        locate_ship(1)
 
-    return f
+    return f, ships
 
 
-def create_empty_field():
+def empty_field():
     f = []
     for i in range(10):
         a = []
@@ -130,3 +70,44 @@ def create_empty_field():
             a.append(None)
         f.append(a)
     return f
+
+
+def field_with_ships(field):
+    f = empty_field()
+    for i in range(10):
+        for k in range(10):
+            s = field[i][k]
+            if s is not None:
+                f[i][k] = 'Ship'
+    return f
+
+
+def surrounding_cells(cell):
+    x, y = cell[0], cell[1]
+    if x == 0:
+        if y == 0:
+            return [cell, (x, y + 1), (x + 1, y), (x + 1, y + 1)]
+        elif y == 9:
+            return [cell, (x + 1, y), (x, y - 1), (x + 1, y - 1)]
+        else:
+            return [cell, (x + 1, y), (x, y - 1),
+                    (x, y + 1), (x + 1, y + 1), (x + 1, y - 1)]
+    elif x == 9:
+        if y == 0:
+            return [cell, (x - 1, y), (x - 1, y + 1), (x, y + 1)]
+        elif y == 9:
+            return [cell, (x, y - 1), (x - 1, y - 1), (x - 1, y)]
+        else:
+            return [cell, (x - 1, y), (x, y - 1), (x, y + 1),
+                    (x - 1, y - 1), (x - 1, y + 1)]
+
+    elif y == 0:
+        return [cell, (x - 1, y), (x + 1, y), (x, y + 1),
+                (x - 1, y + 1), (x + 1, y + 1)]
+    elif y == 9:
+        return [cell, (x - 1, y), (x + 1, y), (x, y - 1),
+                (x - 1, y - 1), (x + 1, y - 1)]
+    else:
+        return [cell, (x - 1, y), (x + 1, y), (x, y - 1),
+                (x, y + 1), (x - 1, y - 1), (x - 1, y + 1),
+                (x + 1, y + 1), (x + 1, y - 1)]
