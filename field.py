d4dea68cecf01926c01ce5064aa28c68be0c2a7e
ipopovych <32884361+ipopovych@users.noreply.github.com>
2018-03-07 16:54:32 +0200

    changed interface

--- field.py
+++ field.py
@@ -1,39 +1,60 @@
-from funcs import create_field, create_empty_field
-from ship import Ship
+from funcs import create_field, empty_field, field_with_ships
 
 
 class Field:
     def __init__(self):
-        self._field = create_field()
-        self._noships = create_empty_field()
-        self._withships = create_empty_field()
-        print(self._field)
+        self._field, self.ships = create_field()
+        self._withships = field_with_ships(self._field)
 
     def shoot_at(self, cell):
-        self._noships[cell[0] - 1][cell[1] - 1] = 'Shot'
-        ship = self._field[cell[0] - 1][cell[1] - 1]
-
+        ship = self._field[cell[0]][cell[1]]
         if ship:
             ship.shoot_at(cell)
+            self._withships[cell[0]][cell[1]] = 'Shotship'
         else:
-            self._field[cell[0] - 1][cell[1] - 1] = 'Shot'
+            self._withships[cell[0]][cell[1]] = 'Shot'
 
     def field_without_ships(self):
-        s = ''
-        for f in self._noships:
+        s = '      A   B   C    D    E   F    G   H   I    J  \n'
+        r = ['1 ', '2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10']
+        for i, f in enumerate(self._withships):
+            s += ' ' + r[i] + ' '
             for c in f:
-                if c:
-                    s += '■'
+                if c == 'Shot':
+                    s += ' 💣 '
+                elif c == 'Ship':
+                    s += ' 🔳 '
+                elif c == 'Shotship':
+                    s += ' ❌ '
                 else:
-                    s += '□'
+                    s += ' 🔳 '
             s += '\n'
         return s
 
     def field_with_ships(self):
-        for c in range(10):
-            for e in range(10):
-            #i = self._withships.index(c)
-                if isinstance(self._field[c, e], Ship):
-                    pass
-        pass
+        s = '      A   B   C    D    E   F    G   H   I    J  \n'
+        r = ['1 ', '2 ', '3 ',  '4 ', '5 ', '6 ', '7 ', '8 ', '9 ', '10']
+        for i, f in enumerate(self._withships):
+            s += ' '+r[i]+' '
+            for c in f:
+                if c == 'Shot':
+                    s += ' 💣 '
+                elif c == 'Ship':
+                    s += ' 🚢 '
+                elif c == 'Shotship':
+                    s += ' ❌ '
+                else:
+                    s += ' 🔳 '
+            s += '\n'
+        return s
 
+    def field(self):
+        return self.field
+
+
+f = Field()
+print(f.field_with_ships())
+print(f.field_without_ships())
+f.shoot_at((0, 4))
+print(f.field_without_ships())
+print(f.field_with_ships())
